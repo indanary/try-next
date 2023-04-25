f3c41255e40c77c1d228a6f483da4bc3e5ab923a
indanary <indanary@gmail.com>
2023-04-25 11:33:12 +0700

    use link component for navigation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,19 @@
+import Link from "next/link"
+
 interface HomeProps {}
 
 const Home: React.FC<HomeProps> = ({}) => {
-	return <div>Home</div>
+	return (
+		<>
+			<div>Home</div>
+			<Link href="/blog" legacyBehavior>
+				<a>Blog</a>
+			</Link>
+			<Link href="/product" legacyBehavior>
+				<a>Products</a>
+			</Link>
+		</>
+	)
 }
 
 export default Home
--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -1,7 +1,38 @@
-interface ProductListProps {}
+import Link from "next/link"
 
-const ProductList: React.FC<ProductListProps> = ({}) => {
-	return <div>ProductList</div>
+interface ProductListProps {
+	productId: number
+}
+
+const ProductList: React.FC<ProductListProps> = ({productId = 100}) => {
+	return (
+		<>
+			<Link href="/" legacyBehavior>
+				Home
+			</Link>
+			<div>ProductList</div>
+			<h2>
+				<Link href="/product/1" legacyBehavior>
+					<a>Product 1</a>
+				</Link>
+			</h2>
+			<h2>
+				<Link href="/product/2" legacyBehavior>
+					<a>Product 2</a>
+				</Link>
+			</h2>
+			<h2>
+				<Link href="/product/3" legacyBehavior replace>
+					<a>Product 3</a>
+				</Link>
+			</h2>
+			<h2>
+				<Link href={`/product/${productId}`} legacyBehavior>
+					<a>Product {productId}</a>
+				</Link>
+			</h2>
+		</>
+	)
 }
 
 export default ProductList
