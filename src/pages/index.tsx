5411e3c6aea5da52e89f62701577365eabeb6ca0
indanary <indanary@gmail.com>
2023-04-25 11:36:32 +0700

    navigating programmatically using useRouter

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,15 @@
 import Link from "next/link"
+import {useRouter} from "next/router"
 
 interface HomeProps {}
 
 const Home: React.FC<HomeProps> = ({}) => {
+	const router = useRouter()
+
+	const handleClick = () => {
+		router.push("/product")
+	}
+
 	return (
 		<>
 			<div>Home</div>
@@ -12,6 +19,7 @@ const Home: React.FC<HomeProps> = ({}) => {
 			<Link href="/product" legacyBehavior>
 				<a>Products</a>
 			</Link>
+			<button onClick={handleClick}>Place Order</button>
 		</>
 	)
 }
