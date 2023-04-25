de11edd0d569c654f60dcb3058bbeb8b3bd7c893
indanary <indanary@gmail.com>
2023-04-25 17:47:32 +0700

    create user component

--- src/pages/users.tsx
+++ src/pages/users.tsx
@@ -1,19 +1,20 @@
+import User from "../../components/user"
+
 interface UserListProps {
-  users: any
+	users: any
 }
 
 const UserList: React.FC<UserListProps> = ({users}) => {
 	return (
-    <>
-      <h1>List of Users</h1>
-      {users.map((user: any, index: number) => (
-        <div key={user.id}>
-          <p>{user.name}</p>
-          <p>{user.email}</p>
-        </div>
-      ))}
-    </>
-  )
+		<>
+			<h1>List of Users</h1>
+			{users.map((user: any, index: number) => (
+				<div key={user.id}>
+					<User user={user} />
+				</div>
+			))}
+		</>
+	)
 }
 
 export default UserList
@@ -22,9 +23,9 @@ export async function getStaticProps() {
 	const response = await fetch("https://jsonplaceholder.typicode.com/users")
 	const data = await response.json()
 
-  return {
-    props: {
-      users: data
-    }
-  }
+	return {
+		props: {
+			users: data,
+		},
+	}
 }
