b6cfeb3f83b9a17b393d1cc3d5b6e549833d78dc
indanary <indanary@gmail.com>
2023-04-25 17:44:38 +0700

    static generation with getStaticProps

---
+++ src/pages/users.tsx
@@ -0,0 +1,30 @@
+interface UserListProps {
+  users: any
+}
+
+const UserList: React.FC<UserListProps> = ({users}) => {
+	return (
+    <>
+      <h1>List of Users</h1>
+      {users.map((user: any, index: number) => (
+        <div key={user.id}>
+          <p>{user.name}</p>
+          <p>{user.email}</p>
+        </div>
+      ))}
+    </>
+  )
+}
+
+export default UserList
+
+export async function getStaticProps() {
+	const response = await fetch("https://jsonplaceholder.typicode.com/users")
+	const data = await response.json()
+
+  return {
+    props: {
+      users: data
+    }
+  }
+}
