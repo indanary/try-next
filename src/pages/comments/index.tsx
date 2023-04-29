c5a861286d87fc6922556cc900558840b3634a6c
indanary <indanary@gmail.com>
2023-04-29 09:12:51 +0700

    comment post request

--- src/pages/comments/index.tsx
+++ src/pages/comments/index.tsx
@@ -4,6 +4,7 @@ interface CommentsPageProps {}
 
 const CommentsPage: React.FC<CommentsPageProps> = ({}) => {
 	const [comments, setComments] = useState([])
+	const [newComment, setNewComment] = useState("")
 
 	const fetchComments = async () => {
 		const response = await fetch("/api/comments")
@@ -12,8 +13,27 @@ const CommentsPage: React.FC<CommentsPageProps> = ({}) => {
 		if (data) setComments(data)
 	}
 
+	const submitComment = async () => {
+		const response = await fetch("/api/comments", {
+			method: "POST",
+			body: JSON.stringify({newComment}),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		})
+		const data = await response.json()
+	}
+
 	return (
 		<>
+			<input
+				type="text"
+				value={newComment}
+				onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+					setNewComment(e.target.value)
+				}
+			/>
+			<button onClick={submitComment}>Submit Comment</button>
 			<button onClick={fetchComments}>Fetch Comments</button>
 			{comments.map((comment: any) => (
 				<div key={comment.id}>
