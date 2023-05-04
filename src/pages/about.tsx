ad1d90ffd3cf724f4855b7eb491713df66e12476
indanary <indanary@gmail.com>
2023-05-04 12:36:44 +0700

    clean project

--- src/pages/about.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "../styles/About.module.scss"
-
-interface AboutProps {}
-
-const About: React.FC<AboutProps> = ({}) => {
-	return <div className={styles.highlightscss}>About Page</div>
-}
-
-export default About
--- src/pages/contact.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "../styles/Contact.module.scss"
-
-interface ContactProps {}
-
-const Contact: React.FC<ContactProps> = ({}) => {
-	return <div className={styles.highlightscss}>Contact Page</div>
-}
-
-export default Contact
