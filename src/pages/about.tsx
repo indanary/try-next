ca1e2657b993ab4e7e1bd07516602d5066aef1ff
indanary <indanary@gmail.com>
2023-05-04 11:41:26 +0700

    apply global styles

---
+++ src/pages/about.tsx
@@ -0,0 +1,7 @@
+interface AboutProps {}
+
+const About: React.FC<AboutProps> = ({}) => {
+	return <button className="btn btn-primary">Primary</button>
+}
+
+export default About
---
+++ src/pages/contact.tsx
@@ -0,0 +1,7 @@
+interface ContactProps {}
+
+const Contact: React.FC<ContactProps> = ({}) => {
+	return <h2>Contact</h2>
+}
+
+export default Contact
