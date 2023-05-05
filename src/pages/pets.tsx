87ca46a872e305c305020ba77795d2ee2537163b
indanary <indanary@gmail.com>
2023-05-05 21:57:01 +0700

    use placeholder blur

--- src/pages/pets.tsx
+++ src/pages/pets.tsx
@@ -1,10 +1,18 @@
 import Image from "next/image"
+import img from "../../public/1.jpeg"
 
 interface PetsPageProps {}
 
 const PetsPage: React.FC<PetsPageProps> = ({}) => {
 	return (
 		<div>
+      <Image
+        src={img}
+        placeholder="blur"
+        alt="pet"
+        width={"280"}
+        height={"420"}
+      />
 			{["1", "2", "3", "4", "5"].map((path: any) => (
 				<div key={path}>
 					<Image
